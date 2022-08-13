bed68813ccfe1b9ea8fac8754497bda0e64bb25b
kistf001 <kistf001@gmail.com>
2022-08-13 20:30:36 +0900

    a

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from algorithm import agent1 as agent
+
 agent.torch.set_num_threads(8)
 
 A = agent.Agent()
 
 for dd in range(0,100000):
 
+    print(dd,' ', end=" ")
+    
     A.run_data()
     
     if(dd %5)==0:
+
         A.param_export()
